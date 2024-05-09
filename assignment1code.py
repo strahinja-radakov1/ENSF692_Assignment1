96c055a6e959950b7830dca4d67469bd47d34490
strahinja-radakov1 <strahinja.radakov1@ucalgary.ca>
2024-05-09 09:14:51 -0600

    Initial commit

---
+++ assignment1code.py
@@ -0,0 +1,12 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Strahinja Radakovic
+
+studentName = "Strahinja Radakovic"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+
+print("My best skill is modelling in Revit.")
